0511962d484d678a6841d8e6db7387f5cba82c4f
Igor Kozachuk <igorkozachuk@gmail.com>
2023-10-16 16:36:48 +0300

    небольшой рефакторинг interests_service

--- mytlg/servises/interests_service.py
+++ mytlg/servises/interests_service.py
@@ -6,9 +6,8 @@ class InterestsService:
 
     @staticmethod
     def get_active_interests(bot_user):
-        active_interests = (Interests.objects.filter(bot_user=bot_user, is_active=True, interest_type='main')
-                            .only('pk', 'is_active'))
-        return active_interests
+        return (Interests.objects.filter(bot_user=bot_user, is_active=True, interest_type='main')
+                .only('pk', 'is_active'))
 
     @staticmethod
     def set_is_active_false_in_active_interests(active_interests):
@@ -18,25 +17,27 @@ class InterestsService:
 
     @staticmethod
     def create_list_of_new_interests_obj(interests_indxs, request):
-        new_interests_objs = [
+        return [
             dict(
                 interest=request.POST.get(f"interest{indx + 1}"),
                 send_period=request.POST.get(f"send_period{indx + 1}"),
-                when_send=datetime.datetime.strptime(request.POST.get(f"when_send{indx + 1}")[:5], '%H:%M').time()
-                if request.POST.get(f"when_send{indx + 1}") else None,
+                when_send=datetime.datetime.strptime(
+                    request.POST.get(f"when_send{indx + 1}")[:5], '%H:%M'
+                ).time()
+                if request.POST.get(f"when_send{indx + 1}")
+                else None,
                 last_send=datetime.datetime.now(),
             )
             for indx in interests_indxs
         ]
-        return new_interests_objs
 
     @staticmethod
     def check_for_having_interests(interests_examples, request):
-        interests_indxs = []
-        for i in range(len(interests_examples)):
-            if request.POST.get(f"interest{i + 1}") != '':
-                interests_indxs.append(i)
-        return interests_indxs
+        return [
+            i
+            for i in range(len(interests_examples))
+            if request.POST.get(f"interest{i + 1}") != ''
+        ]
 
     @staticmethod
     def get_send_periods():
