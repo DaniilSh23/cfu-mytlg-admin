65432e4bc2d120db9e97cd3d4f303bb16a4a0d1e
DaniilSh23 <danyasevas11@gmail.com>
2023-12-08 10:55:21 +0300

    правка условия перед отправкой уведомлений о подписке

--- mytlg/views.py
+++ mytlg/views.py
@@ -513,7 +513,8 @@ class WriteSubsResults(APIView):
                 MY_LOGGER.debug(f'Обновляем данные в БД по задаче аккаунта c PK=={task_obj.pk}')
                 AccountsSubscriptionTasksService.update_task_obj_data(ser, task_obj)
 
-                if task_obj.tlg_acc.acc_tlg_id:
+                # Если у таски есть связь с юзером бота
+                if task_obj.assigned_user.tlg_id:
                     success_subscription = True if int(ser.validated_data.get("success_subs")) > 0 else False
                     # Отправка уведомления юзеру
                     AccountsSubscriptionTasksService.send_subscription_notification(
