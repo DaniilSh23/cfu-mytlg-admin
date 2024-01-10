9aef0f4152085c7b19cb579a95e017dc85f09b23
DaniilSh23 <danyasevas11@gmail.com>
2024-01-10 13:58:55 +0300

    сократил до недели выборку постов при обработке нового поста

--- mytlg/servises/news_posts_service.py
+++ mytlg/servises/news_posts_service.py
@@ -1,9 +1,11 @@
+import pytz
+
 from mytlg.models import NewsPosts
 from mytlg.servises.bot_settings_service import BotSettingsService
 import datetime
 from django.db.models import QuerySet
 from django.core.exceptions import ObjectDoesNotExist
-from cfu_mytlg_admin.settings import MY_LOGGER
+from cfu_mytlg_admin.settings import MY_LOGGER, TIME_ZONE
 
 
 class NewsPostsService:
@@ -12,9 +14,12 @@ class NewsPostsService:
     def get_posts_only_text_and_embeddings_by_channels_ids_list(ch_ids_lst):
         """
         Фильтрация постов из списка каналов и которые пришли не из кастомных каналов пользователя.
+        Также дата создания этих постов должна быть не позднее 7 дней.
         Достаем для постов только text, embedding.
         """
-        return NewsPosts.objects.filter(channel__id__in=ch_ids_lst, from_custom_channel=False).only("text", "embedding")
+        week_early_dt = datetime.datetime.now(tz=pytz.timezone(TIME_ZONE)) - datetime.timedelta(weeks=1)
+        return NewsPosts.objects.filter(channel__id__in=ch_ids_lst, from_custom_channel=False,
+                                        created_at__gte=week_early_dt).only("text", "embedding")
 
     @staticmethod
     def create_news_post(ch_obj, ser, short_post):
