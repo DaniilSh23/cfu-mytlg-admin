096bc5efdadc870d23fe5b8cb924047d2ce3b72a
Igor Kozachuk <igorkozachuk@gmail.com>
2024-01-12 13:15:52 +0300

    рабочий процесс2

--- support/services/support_messages_service.py
+++ support/services/support_messages_service.py
@@ -1,9 +1,19 @@
 from support.models import SupportMessages
 from django.core.exceptions import ObjectDoesNotExist
+
+from mytlg.servises.bot_users_service import BotUsersService
 from cfu_mytlg_admin.settings import MY_LOGGER
 
 
 class SupportMessagesService:
 
+    @staticmethod
+    def create_message(message_data: dict):
+        message = SupportMessages(
+            bot_user=message_data.get('bot_user'),
+            message=message_data.get('message')
+        )
+        message.save()
+        return message
 
 
--- support/views.py
+++ support/views.py
@@ -5,11 +5,24 @@ from rest_framework.request import Request
 
 from cfu_mytlg_admin.settings import MY_LOGGER, BOT_TOKEN
 from support.serializers import SupportMessageSerializer
-from support.services.support_messages_service import
+from support.services.support_messages_service import SupportMessagesService
+from mytlg.servises.check_request_services import CheckRequestService
 
 
 class SupportMessages(View):
 
-    def get(self):
+    def get(self, request):
 
         pass
+
+
+    def post(self, request):
+        MY_LOGGER.info('Получен запрос на вьюшку приёма сообщений в саппорт.')
+        request_data = SupportMessageSerializer(data=request.data)
+
+        # Проверка токена
+        CheckRequestService.check_bot_token(token=request.data.get("token"))
+
+        if request_data.is_valid():
+            validated_data = request_data.validated_data
+            return Response(status=200, data={'result': 'OK!'})
